218f9c92b65ff54f5dbd82ea5d5566f3f0d100df
Max Margenot <max.margenot@gmail.com>
2020-05-19 19:21:26 -0400

    MAINT: added initial setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='MCWI',
+    version='0.0.1',
+    description='Synthetic data streaming for testing time series models.',
+    url='https://github.com/mmargenot/mcwi',
+    author='Max Margneot',
+    author_email='max.margenot@gmail.com',
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    install_requires=[]
+)
