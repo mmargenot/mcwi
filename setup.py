4ec46a19b93d1d60d5d4d073dddea3219404e245
Max Margenot <max.margenot@gmail.com>
2020-05-19 19:33:14 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     version='0.0.1',
     description='Synthetic data streaming for testing time series models.',
     url='https://github.com/mmargenot/mcwi',
-    author='Max Margneot',
+    author='Max Margenot',
     author_email='max.margenot@gmail.com',
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
