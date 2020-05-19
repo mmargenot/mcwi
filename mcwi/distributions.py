26a4dc23cb1e0b429d9508b1ff66706a1d402c4d
Max Margenot <max.margenot@gmail.com>
2020-05-19 19:26:57 -0400

    ENH: started random walk

--- mcwi/distributions.py
+++ mcwi/distributions.py
@@ -1,13 +1,22 @@
 import numpy as np
+import abc
 
 
-class DataType:
+class DataType(abc.ABCMeta):
 
     def __init__(self):
         pass
 
+    @abc.abstractmethod
     def sample_one(self):
         pass
 
+    @abc.abstractmethod
     def sample_n(self, n):
+        pass
+
+
+class RandomWalk(DataType):
+
+    def __init__(self, params):
         pass
--- mcwi/mcwi.py
+++ mcwi/mcwi.py
@@ -1,6 +1,14 @@
+import distributions
+
+
 class DataStream:
-    pass
+    """A "live" stream of synthetic data.
+    """
+    def __init__(self, data: distributions.DataType):
+        pass
 
 
 class DataSet:
+    """Sampled dataset with no live stream.
+    """
     pass
