e84af3c8f77b41aaa8a60cdf5a47e68c6859d1b7
Max Margenot <max.margenot@gmail.com>
2020-05-21 11:24:11 -0400

    DOC: include docs for base DataType

--- mcwi/distributions.py
+++ mcwi/distributions.py
@@ -9,33 +9,62 @@ class DataType(abc.ABCMeta):
 
     @abc.abstractmethod
     def step_forward_one(self):
+        """Move forward a single step in a random walk according to the
+        parameters of the distribution.
+
+        Returns
+        -------
+        next_step : float
+            The next step in the random walk, to be served to the consumer.
+        """
         pass
 
     @abc.abstractmethod
     def step_forward_n(self, n):
+        """Move forward n steps in a random walk according to the parameters
+        of the distribution.
+
+        Parameters
+        ----------
+        n : int
+            Number of steps to move forward.
+
+        Returns
+        -------
+        next_step : float
+            The next step in the random walk, to be served to the consumer.
+        """
         pass
 
 
 class BrownianMotion(DataType):
     """A simple Brownian motion. The standard deviation of the normally-distributed
     next sampled step is equal to the time difference.
+
+    See Also
+    --------
+    DataType
     """
 
     def __init__(self, init, mu=0, sigma=1):
-        self.init = init
+        self.current_value = init
         self.mu = mu
         self.sigma = sigma
 
     def step_forward_one(self):
-        next_step = self.init + np.random.normal(
+        next_step = self.current_value + np.random.normal(
             mu=self.mu,
             sigma=self.sigma)
+
+        self.current_value = next_step
 
         return next_step
 
     def step_forward_n(self, n):
-        next_step = self.init + np.random.normal(
+        next_step = self.current_value + np.random.normal(
             mu=self.mu,
             sigma=self.sigma*n)
+
+        self.current_value = next_step
 
         return next_step
